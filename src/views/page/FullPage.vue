06f1123d51715237aafefb15f80db2fec59f55cb
Ayush177 <ayusharora17785@gmail.com>
2021-02-17 17:34:33 +0530

    Corrected dates

--- src/views/page/FullPage.vue
+++ src/views/page/FullPage.vue
@@ -1,22 +1,29 @@
 <template>
   <div>
-    <the-navbar class="mb-base"/>
-    <stats style="margin-top: 125px" :income="income" :expense="expense" :transactions="transactions"/>
-    <balance :income="income" :expense="expense"/>
+    <the-navbar class="mb-base" />
+    <stats
+      style="margin-top: 125px"
+      :income="income"
+      :expense="expense"
+      :transactions="transactions"
+    />
+    <balance :income="income" :expense="expense" />
     <div class="text-center">
-    <b-button v-b-modal.modal-center variant="primary" class="shadow rounded">Add New Transaction</b-button>
+      <b-button v-b-modal.modal-center variant="primary" class="shadow rounded"
+        >Add New Transaction</b-button
+      >
     </div>
-    <new-transaction @update="update"/>
-    <transaction-tabs :transactions="transactions"/>
+    <new-transaction @update="update" />
+    <transaction-tabs :transactions="transactions" />
   </div>
 </template>
 
 <script>
-import TheNavbar from '../../components/nav/TheNavbar.vue';
-import Stats from '../../components/statistics/Stats.vue';
-import Balance from '../../components/details/Balance.vue';
-import TransactionTabs from '../../components/transactions/TransactionTabs.vue';
-import NewTransaction from '../../components/transactions/NewTransaction.vue';
+import TheNavbar from "../../components/nav/TheNavbar.vue";
+import Stats from "../../components/statistics/Stats.vue";
+import Balance from "../../components/details/Balance.vue";
+import TransactionTabs from "../../components/transactions/TransactionTabs.vue";
+import NewTransaction from "../../components/transactions/NewTransaction.vue";
 
 export default {
   name: "App",
@@ -25,76 +32,74 @@ export default {
     Stats,
     Balance,
     TransactionTabs,
-    NewTransaction
+    NewTransaction,
   },
-  data(){
+  data() {
     return {
       income: 100100,
       expense: 22550,
-      transactions:[
-            {
-                startdate: '2021-01-21',
-                enddate: '2021-01-21',
-                type: 'income',
-                category: "Salary",
-                duration: "monthly",
-                item: "Salary",
-                description: "I got my salary",
-                amount: 100000,
-            },
-            {
-                startdate: '2021-02-01',
-                enddate: '2021-02-01',
-                type: 'expense',
-                category: "Shopping",
-                duration: "monthly",
-                item: "Grocery",
-                description: "Grocery Shopping",
-                amount: -20000,
-            },
-            {
-                startdate: '2021-01-21',
-                enddate: '2021-01-21',
-                type: 'expense',
-                category: "Food & Drinks",
-                duration: "daily",
-                item: "Pizza",
-                description: "I love Pizzas",
-                amount: -1550,
-            },
-            {
-                startdate: '2021-01-21',
-                enddate: '2021-01-21',
-                type: 'expense',
-                category: "Entertainment",
-                duration: "weekly",
-                item: "Movie",
-                description: "Let's watch some movies",
-                amount: -2000,
-            },
-            {
-                startdate: '2021-01-21',
-                enddate: '2021-01-21',
-                type: 'income',
-                category: "Award Wins",
-                duration: "daily",
-                item: "Hackathon Win",
-                description: "I won a Hackathon",
-                amount: 1000,
-            },
-        ],
-    }
+      transactions: [
+        {
+          startdate: "2021-01-21",
+          enddate: "2021-02-21",
+          type: "income",
+          category: "Salary",
+          duration: "monthly",
+          item: "Salary",
+          description: "I got my salary",
+          amount: 100000,
+        },
+        {
+          startdate: "2021-02-01",
+          enddate: "2021-03-01",
+          type: "expense",
+          category: "Shopping",
+          duration: "monthly",
+          item: "Grocery",
+          description: "Grocery Shopping",
+          amount: -20000,
+        },
+        {
+          startdate: "2021-01-21",
+          enddate: "2021-01-21",
+          type: "expense",
+          category: "Food & Drinks",
+          duration: "daily",
+          item: "Pizza",
+          description: "I love Pizzas",
+          amount: -1550,
+        },
+        {
+          startdate: "2021-01-21",
+          enddate: "2021-01-28",
+          type: "expense",
+          category: "Entertainment",
+          duration: "weekly",
+          item: "Movie",
+          description: "Let's watch some movies",
+          amount: -2000,
+        },
+        {
+          startdate: "2021-01-21",
+          enddate: "2021-01-21",
+          type: "income",
+          category: "Award Wins",
+          duration: "daily",
+          item: "Hackathon Win",
+          description: "I won a Hackathon",
+          amount: 1000,
+        },
+      ],
+    };
   },
   methods: {
-    update(data){
-        if(data.type=='expense'){
-          this.expense=parseInt(this.expense)+parseInt(data.amount);
-          data.amount*=-1;
-        }
-        else
-          this.income=parseInt(this.income)+parseInt(data.amount);
-        this.transactions.push(data);
-    }
+    update(data) {
+      if (data.type == "expense") {
+        this.expense = parseInt(this.expense) + parseInt(data.amount);
+        data.amount *= -1;
+      } else this.income = parseInt(this.income) + parseInt(data.amount);
+      this.transactions.push(data);
+    },
   },
   mounted() {
     if (localStorage.income) {
@@ -104,7 +109,9 @@ export default {
       this.expense = localStorage.expense;
     }
     if (localStorage.transactions) {
-      this.transactions = JSON.parse(localStorage.getItem("transactions") || "[]");
+      this.transactions = JSON.parse(
+        localStorage.getItem("transactions") || "[]"
+      );
     }
   },
   watch: {
@@ -114,9 +121,9 @@ export default {
     expense(newExpense) {
       localStorage.expense = newExpense;
     },
-    transactions(newTransactions){
+    transactions(newTransactions) {
       localStorage.setItem("transactions", JSON.stringify(newTransactions));
-    }
-  }
+    },
+  },
 };
 </script>
